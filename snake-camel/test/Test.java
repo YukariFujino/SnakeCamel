457afcd15c6a4dddf20f924ac87a74ae53f83de6
g13904fy <g13904fy@s30410.tsuda.ac.jp>
2016-05-06 12:54:19 +0900

    エラーを解決しました！

--- snake-camel/test/Test.java
+++ snake-camel/test/Test.java
@@ -1,10 +1,23 @@
+import snakecamel.SnakeCamelUtil;
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 public class Test {
 
 	@org.junit.Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSC() {
+		SnakeCamelUtil util=new SnakeCamelUtil();
+		String scCase= util.snakeToCamelcase("hello_world");
+		String expected="HelloWorld";
+		assertThat(scCase,is(expected));
 	}
+	@org.junit.Test
+	public void testCS() {
+		SnakeCamelUtil util=new SnakeCamelUtil();
+		String csCase= util.camelToSnakecase("GoodBye");
+		String expected="good_bye";
+		assertThat(csCase,is(expected));
+	}
+
 
 }
